161da127b84d80d34be3a304ca2836477fa72dac
mostafaIn <mostafa2015se@gmail.com>
2022-05-13 16:33:49 +0300

    setup firebase and storing recipes data into a realtime database

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Recipe } from '../recipes/recipe.model';
+import { RecipeService } from '../recipes/recipe.service';
+
+@Injectable({ providedIn: 'root' })
+export class DataStorageService {
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes() {
+    const recipes: Recipe[] = this.recipeService.getRecipes();
+    this.http
+      .put<Recipe[]>(
+        'https://ng-recipes-book-a35f7-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
+        recipes
+      )
+      .subscribe((res) => console.log(res));
+  }
+}
